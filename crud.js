1d6126855d59f59a75865e8bc6923d7e01e5f445
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-09-29 20:29:01 +0200

    crud async implementations wrapped in try/catch

--- crud.js
+++ crud.js
@@ -1,44 +1,64 @@
 const uuid = require('uuid')
 let users = [];
 async function getUsers(req, res, next) {
-	await res
-		.status(200)
-		.json({ users })
+	try {
+		await res
+			.status(200)
+			.json({ users })
+	} catch (error) {
+		console.log(`Error ${error}`);
+	}
 }
 async function getUser(req, res, next) {
-	const user = users.find(user => user.id === req.params.id)
-	console.log(user);
-	await res
-		.status(200)
-		.json(user)
+	try {
+		const user = users.find(user => user.id === req.params.id)
+		console.log(user);
+		await res
+			.status(200)
+			.json(user)
+	} catch (error) {
+		console.error(`Error ${error}`)
+	}
 }
 async function createUser(req, res, next) {
-	const newUser = {
-		id: uuid.v4(),
-		name: req.body.name,
-		timeStamp: req.locals
+	try {
+		const newUser = {
+			id: uuid.v4(),
+			name: req.body.name,
+			timeStamp: req.locals
+		}
+		users.push(newUser)
+		res.status(201).json({ newUser });
+	} catch (error) {
+		console.error(`Error ${error}`)
 	}
-	users.push(newUser)
-	res.status(201).json({ newUser });
 }
 async function deleteUser(req, res, next) {
-	const id = req.params.id;
-	const userId = users.findIndex(u => u.id === id);
-	const usrs = [...users.slice(0, userId), ...users.slice(userId + 1)]
-	users = usrs;
-	res.json({ usrs });
+	try {
+		const id = await req.params.id;
+		const userId = users.findIndex(u => u.id === id);
+		const usrs = [...users.slice(0, userId), ...users.slice(userId + 1)]
+		users = usrs;
+		res.json({ usrs });
+	} catch (error) {
+		console.error(`Error ${error}`)
+	}
 }
 
 async function checkIfUserExistsInDatabase(req, res, next) {
-	const name = await req.body.name;
-	for (let user of users) {
-		if (user.name === name) {
-			return res.json({
-				message: 'User already exists'
-			})
+	try {
+		const name = await req.body.name;
+		for (let user of users) {
+			if (user.name === name) {
+				return res.json({
+					message: 'User already exists'
+				})
+			}
 		}
+		next();
+	} catch (error) {
+		console.error(`Erorr ${error}`)
 	}
-	next();
 }
 
 module.exports = { getUser, deleteUser, getUsers, createUser, checkIfUserExistsInDatabase }
