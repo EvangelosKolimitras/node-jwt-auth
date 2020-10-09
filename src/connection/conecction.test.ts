ee2935b26d5f8e499cf2e334a7d68b1eec12ae29
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-10-09 17:16:47 +0200

    automated connection tests wrapped in a parent descript function

--- src/connection/conecction.test.ts
+++ src/connection/conecction.test.ts
@@ -1,30 +1,30 @@
 import connection, { createUrlString, connectToClient } from './connection'
 
-describe('Function createUrlString', () => {
-	it('should return a string', () => {
-		const uri = createUrlString();
-		expect(typeof uri).toBe("string")
-	})
-	it('and the string should contain the words "mongodb+srv" and "mongodb.net"', () => {
-		const uri = createUrlString();
-		[".mongodb.net", "mongodb+srv"].forEach(el => {
-			expect(uri).toContain(el)
+describe('Connection', () => {
+	describe('createUrlString', () => {
+		it('should return a string', () => {
+			const uri = createUrlString();
+			expect(typeof uri).toBe("string")
+		})
+		it('and the string should contain the words "mongodb+srv" and "mongodb.net"', () => {
+			const uri = createUrlString();
+			[".mongodb.net", "mongodb+srv"].forEach(el => expect(uri).toContain(el))
 		})
 	})
-})
 
-describe('Function connectToClient', () => {
-	it('should return an object with the method db', async () => {
-		const client = await connectToClient();
-		expect(typeof client).toBe("object")
-		expect(client).resolves.not.toBeFalsy
-		expect(client).toHaveProperty("db")
+	describe('connectToClient', () => {
+		it('should return an object with the method db', async () => {
+			const client = await connectToClient();
+			expect(typeof client).toBe("object")
+			expect(client).resolves.not.toBeFalsy
+			expect(client).toHaveProperty("db")
+		})
 	})
-})
 
-describe('Function connection', () => {
-	it('should return the users database', async () => {
-		const con = await connection();
-		expect(typeof con).toBe("object")
+	describe('connection', () => {
+		it('should return the users database', async () => {
+			const con = await connection();
+			expect(typeof con).toBe("object")
+		})
 	})
 })
